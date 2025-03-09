7f056a898f44f14c7f25e4d65ce6238f91f4f090
dogeonkim1 <caddibo4@naver.com>
2025-03-09 16:16:46 +0900

    style: 배경 색상 취소

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ProfileForm from './pages/profile/ProfileForm.tsx';
 const App = () => {
     return (
         <BrowserRouter>
-            <div style={{ minHeight: "100vh", width: "100%", backgroundColor: "#7391e9" }}>
+            <div className="min-h-screen bg-gray-50 w-full">
                 <div className="w-full px-4 sm:px-6 lg:px-8 py-8">
                     <Routes>
                         <Route path="/" element={<HomePage />} />
